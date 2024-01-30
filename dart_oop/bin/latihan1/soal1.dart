bf48264dd8edb6bfa9f66ef8c28490fce1559a09
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-01-30 09:53:43 +0700

    latihan 1

---
+++ dart_oop/bin/latihan1/soal1.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+class Mobil {
+  String? merk;
+  String? model;
+  int? tahun;
+
+  void display() {
+    print("\nMerk Mobil  : $merk");
+    print("Model MObil : $model");
+    print("Tahun       : $tahun");
+  }
+}
+
+void main() {
+  Mobil mobil = Mobil();
+  stdout.write("Masukkan Merk Mobil   : ");
+  mobil.merk = stdin.readLineSync();
+  stdout.write("Masukkkan Model MObil : ");
+  mobil.model = stdin.readLineSync();
+  stdout.write("Masukkan Tahun MObil  : ");
+  mobil.tahun = int.parse(stdin.readLineSync()!);
+  mobil.display();
+}
