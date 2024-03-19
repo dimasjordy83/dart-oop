d96dd405ea110626f1b0847b091ffd6ccdaec776
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-03-19 22:18:06 +0700

    encapsulation get dan set

---
+++ my_oop/bin/materi/encapsulation_get_set.dart
@@ -0,0 +1,23 @@
+class Vehicle {
+  String? _model;
+  int? _year;
+  // Getter method
+  String? get model => _model;
+
+  // Setter method
+  set model(String? model) => _model = model;
+
+  // Getter method
+  int? get year => _year;
+
+  // Setter method
+  set year(int? year) => _year = year;
+}
+
+void main() {
+  var vehicle = Vehicle();
+  vehicle.model = "Toyota";
+  vehicle.year = 2019;
+  print("mobil : ${vehicle.model}");
+  print("tahun : ${vehicle.year}");
+}
