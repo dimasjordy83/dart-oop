7cdf146aef72df1a5defa9d7b958f277535d91e3
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-03-19 17:58:39 +0700

    constructor pada class

--- my_oop/bin/materi/class.dart
+++ my_oop/bin/materi/class.dart
@@ -2,22 +2,35 @@ class Montor {
   String? merk;
   String? model;
   int? tahun;
+
+  // constructor
+  Montor(String merk, String model, int tahun) {
+    this.merk = merk;
+    this.model = model;
+    this.tahun = tahun;
+  }
 // function
   void klakson() {
     print("tettettttttt");
   }
 
   void main() {
-    Montor montor1 = Montor();
+    // Montor montor1 = Montor();
 
-    montor1.merk = "Supra Bapak";
-    montor1.model = "Bebek";
-    montor1.tahun = 1990;
+    // montor1.merk = "Supra Bapak";
+    // montor1.model = "Bebek";
+    // montor1.tahun = 1990;
 
-    print(montor1.merk); //Output: Supra Bapak
-    print(montor1.model); //Output: Bebek
-    print(montor1.tahun); //Output: 1990
+    // print(montor1.merk); //Output: Supra Bapak
+    // print(montor1.model); //Output: Bebek
+    // print(montor1.tahun); //Output: 1990
 
-    montor1.klakson(); //Output: tettettttttt
+    // montor1.klakson(); //Output: tettettttttt
+
+    Montor montor2 = Montor("Vario", "matic", 2019);
+
+    print(montor2.merk); //Output: Vario
+    print(montor2.model); //Output: matic
+    print(montor2.tahun); //Output: 2019
   }
 }
