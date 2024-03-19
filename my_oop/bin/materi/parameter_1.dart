f415e75472c5f582a976392cee036167e2c28286
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-03-19 19:57:03 +0700

    cth parameter 2

--- my_oop/bin/materi/parameter_1.dart
+++ my_oop/bin/materi/parameter_1.dart
@@ -4,12 +4,23 @@ class Student {
   int? rollNumber;
 
   //constructor
-  Student(this.name, this.age, this.rollNumber);
+  // Student(this.name, this.age, this.rollNumber);
+  Student({String? name, int? age, int? rollNumber}) {
+    this.name = name;
+    this.age = age;
+    this.rollNumber = rollNumber;
+  }
 }
 
 void main() {
-  //student object dari class Student
-  Student student = Student("John", 20, 1);
+  // //student object dari class Student
+  // Student student = Student("John", 20, 1);
+  // print("Name: ${student.name}");
+  // print("Age: ${student.age}");
+  // print("Roll Number: ${student.rollNumber}");
+
+  //parameter dengan name
+  Student student = Student(name: "John", age: 20, rollNumber: 1);
   print("Name: ${student.name}");
   print("Age: ${student.age}");
   print("Roll Number: ${student.rollNumber}");
