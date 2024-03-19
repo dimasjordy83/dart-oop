33f23790ac2e3ff7f8f4116189d625ac92250053
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-03-19 17:52:52 +0700

    object dari class

--- my_oop/bin/materi/class.dart
+++ my_oop/bin/materi/class.dart
@@ -6,4 +6,18 @@ class Montor {
   void klakson() {
     print("tettettttttt");
   }
+
+  void main() {
+    Montor montor1 = Montor();
+
+    montor1.merk = "Supra Bapak";
+    montor1.model = "Bebek";
+    montor1.tahun = 1990;
+
+    print(montor1.merk); //Output: Supra Bapak
+    print(montor1.model); //Output: Bebek
+    print(montor1.tahun); //Output: 1990
+
+    montor1.klakson(); //Output: tettettttttt
+  }
 }
